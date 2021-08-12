5e3aea7e27e461a8edcad767c9188ec08247fbe1
Ignacio Emiliano Juarez <75501977+emij7@users.noreply.github.com>
2021-08-12 19:04:15 -0300

    Fixed URL to deploy 2

--- snowpack.config.js
+++ snowpack.config.js
@@ -6,7 +6,6 @@ module.exports = {
   },
   buildOptions: {
     baseUrl: '',
-    out: 'docs',
     // put the meta snowpack build files under snowpack instead of _snowpack since Github special-cases underscore prefixed folders
     metaUrlPath: 'snowpack'
   },
